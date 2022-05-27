04caf6102a4b27e0b53a9180722de423fa43867f
Macj0427 <macj0427@gmail.com>
2022-05-27 06:48:22 -0700

    canvas login and register

--- src/App.js
+++ src/App.js
@@ -1,18 +1,52 @@
-import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
-import Login from './components/Login';
-import Register from './components/Register';
+import React from 'react';
+import { useCanvas } from './hooks/useCanvas';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="container">
-        <Route exact path="/" component={Login} />
-        <Route path="/register" component={Register} />
+
+function App() {
+  
+  const [ coordinates, setCoordinates, canvasRef, canvasWidth, canvasHeight ] = useCanvas();
+
+  const handleCanvasClick=(event)=>{
+    // on each click get current mouse location 
+    const currentCoord = { x: event.clientX, y: event.clientY };
+    // add the newest mouse location to an array in state 
+    setCoordinates([...coordinates, currentCoord]);
+  };
+
+  const handleClearCanvas=(event)=>{
+    setCoordinates([]);
+  };
+
+  return (
+    <main className="App-main" >
+      <canvas 
+        className="App-canvas"
+        ref={canvasRef}
+        width={canvasWidth}
+        height={canvasHeight}
+        />
+      <div className="login">
+       <input placeholder="Name"></input>
+        <input placeholder="password" type="password"></input>
+        <button>Log in</button>
       </div>
-    );
-  }
-}
+      <div className="register">
+       <input placeholder="Name" />
+       <input placeholder="Email" />
+        <input placeholder="password" type="password" />
+        <input placeholder="confirm-password" type="password" />
+        <button>Register</button>
+      </div>
+        
+        
+
+      <div className="button" >
+        <button onClick={handleClearCanvas} > CLEAR </button>
+      </div>
+    </main>
+  );
+
+};
 
 export default App;
