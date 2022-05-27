b46aca4c5b1322942d2918d5895f114e7cbc288b
Rieser Stern <RieserStern@gmail.com>
2022-05-27 12:17:49 -0400

    del old code

--- src/App.js
+++ src/App.js
@@ -1,50 +1,9 @@
-import React from 'react';
-import { useCanvas } from './hooks/useCanvas';
-import './App.css';
-
+import React from 'react'
+import Canvas from './Canvas'
 
 function App() {
-  
-  const [ coordinates, setCoordinates, canvasRef, canvasWidth, canvasHeight ] = useCanvas();
 
-  const handleCanvasClick=(event)=>{
-    // on each click get current mouse location 
-    const currentCoord = { x: event.clientX, y: event.clientY };
-    // add the newest mouse location to an array in state 
-    setCoordinates([...coordinates, currentCoord]);
-  };
+  return <h1>Hello world!</h1>
+}
 
-  const handleClearCanvas=(event)=>{
-    setCoordinates([]);
-  };
-
-  return (
-    <main className="App-main" >
-      <canvas 
-        className="App-canvas"
-        ref={canvasRef}
-        width={canvasWidth}
-        height={canvasHeight}
-        />
-      <div className="login">
-       <input placeholder="Name"></input>
-        <input placeholder="password" type="password"></input>
-        <button>Log in</button>
-      </div>
-      <div className="register">
-       <input placeholder="Name" />
-       <input placeholder="Email" />
-        <input placeholder="password" type="password" />
-        <input placeholder="confirm-password" type="password" />
-        <button>Register</button>
-      </div>
-      
-      <div className="button" >
-        <button onClick={handleClearCanvas} > CLEAR </button>
-      </div>
-    </main>
-  );
-
-};
-
-export default App;
+export default App
